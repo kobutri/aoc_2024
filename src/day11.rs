119ab711b6079fb3223d4e1288596c5308d3e592
Tristan Kobusch <tristan.kobusch@campus.tu-berlin.de>
2024-12-13 00:40:03 +0100

    day11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,14 +1,20 @@
+use std::collections::{HashMap, VecDeque};
 use itertools::Itertools;
+use num_bigint::{BigUint, ToBigUint};
+use num_traits::One;
 
 fn solve(count: i32) {
     let input = "125 17";
     let input = include_str!("../input/day11_1.txt");
-    let mut input = input.split(" ").map(|s| s.trim().parse::<u128>().unwrap()).collect_vec();
+    let input = input.split(" ").map(|s| s.trim().parse::<u128>().unwrap()).collect_vec();
+    let mut input = input.into_iter().fold(HashMap::new(), |mut map, val| {
+        *map.entry(val).or_insert(0) += 1;
+        map
+    });
     for i in 0..count {
-        println!("{i}, {}", input.len());
-        input = input.into_iter().flat_map(|x| {
+        input = input.into_iter().fold(HashMap::new(), |mut map, (x, count)| {
             if x == 0 {
-                [Some(1), None].into_iter()
+                *map.entry(1).or_insert(0) += count;
             } else {
                 let mut digits = 1;
                 let mut y = x;
@@ -20,19 +26,24 @@ fn solve(count: i32) {
                     let factor = 10u128.pow(digits / 2);
                     let left = x / factor;
                     let right = x % factor;
-                    [Some(left), Some(right)].into_iter()
+                    *map.entry(left).or_insert(0) += count;
+                    *map.entry(right).or_insert(0) += count;
                 } else {
-                    [Some(x * 2024), None].into_iter()
+                    *map.entry(x * 2024).or_insert(0) += count;
                 }
             }
-        }).filter_map(|x| x).collect_vec();
+
+            map
+        });
+        // println!("{}, {}, {:?}", i+1, input.values().sum::<u128>(), input);
     }
 
-    println!("{}", input.len());
+    println!("{}", input.values().sum::<u128>());
 }
 pub fn day11_1() {
     solve(25);
 }
+
 pub fn day11_2() {
     solve(75);
 }
